3c7aaf512cf03d7051f08541f56ad6e1f0afd5bd
Darin McGill <darin.mcgill@gmail.com>
2018-05-02 00:41:02 -0400

    tweaks

--- python/wsgiref/trivial.py
+++ python/wsgiref/trivial.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+
 def trivial(_, start_response):
     start_response('200 OK', [('Content-type', 'text/plain')])
     return [b"Hello world!\n"]
+
 
 if __name__ == "__main__":
     from wsgiref.simple_server import make_server
